01562e68d3a16458e034f4a08fab7b3365e920cc
Administrator <youzhi8d>
2017-12-07 17:31:46 +0800

    Update

---
+++ app/src/main/java/com/github/fanhx/opensourceproject/MyApplication.java
@@ -0,0 +1,11 @@
+package com.github.fanhx.opensourceproject;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class MyApplication extends Application {
+
+}
---
+++ app/src/main/java/com/github/fanhx/opensourceproject/utils/ToastUtil.java
@@ -0,0 +1,10 @@
+package com.github.fanhx.opensourceproject.utils;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class ToastUtil  {
+    private static  final String toast = null;
+
+}
